1b6b2eab53e9d6d08dab93d8e00a9bb1d18fbebe
hugosenari <hugosenari@gmail.com>
2022-05-12 11:54:30 -0300

    chore: re index nim_searches

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."nim_searches-master".dir   = "master";
+  inputs."nim_searches-master".owner = "nim-nix-pkgs";
+  inputs."nim_searches-master".ref   = "master";
+  inputs."nim_searches-master".repo  = "nim_searches";
+  inputs."nim_searches-master".type  = "github";
+  inputs."nim_searches-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nim_searches-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
